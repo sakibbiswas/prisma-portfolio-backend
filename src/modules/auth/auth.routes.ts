0b9d966825eb6cc06b6ed091b6263dccaf5bf7e7
Sakib Biswas <sakibsakib99880@gmail.com>
2025-10-02 17:15:24 +0600

    Auth route  added

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -5,7 +5,7 @@ import { register, login } from "./auth.controller";
 const router = express.Router();
 
 // Public
-router.post("/register", register); // optional - you can remove in production
+router.post("/register", register); 
 router.post("/login", login);
 
 export default router;
