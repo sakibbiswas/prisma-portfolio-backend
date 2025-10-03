691ee7013d142eb9abf404004a94870e65c3000b
Sakib Biswas <sakibsakib99880@gmail.com>
2025-10-03 19:41:02 +0600

    project route  changes

--- src/modules/project/project.routes.ts
+++ src/modules/project/project.routes.ts
@@ -1,39 +1,4 @@
 
-// import express from "express";
-// import * as projectController from "./project.controller";
-// import { authenticate } from "../../middlewares/auth.middleware";
-// import { uploadProjectThumbnail } from "../../middlewares/upload.middleware";
-
-// const router = express.Router();
-
-// // Public
-// router.get("/", projectController.list);
-// router.get("/:id", projectController.getById);
-
-// // Protected
-// router.post(
-//   "/",
-//   authenticate,
-//   uploadProjectThumbnail.single("thumbnail"),
-//   projectController.create
-// );
-// router.put(
-//   "/:id",
-//   authenticate,
-//   uploadProjectThumbnail.single("thumbnail"),
-//   projectController.update
-// );
-// router.delete("/:id", authenticate, projectController.remove);
-
-// export default router;
-
-
-
-
-
-
-
-
 // src/modules/project/project.routes.ts
 import express from "express";
 import * as projectController from "./project.controller";
